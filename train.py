44b2d0e6cd431748d49135c16c7db84f9bd8a541
zero-or-one <48153370+zero-or-one@users.noreply.github.com>
2021-07-21 00:44:35 +0900

    train update

--- train.py
+++ train.py
@@ -7,22 +7,21 @@ def class_accuracy(pred_labels, true_labels):
     return sum(y_pred == y_act for y_pred, y_act in zip(pred_labels, true_labels))
 
 # L1 regularization
-def l1_loss(model,model_init,weight_decay):
-    l2_loss = 0
-    for (k,p),(k_init,p_init) in zip(model.named_parameters(),model_init.named_parameters()):
-        if p.requires_grad:
-            l2_loss +=  (p-p_init).pow(2).sum()
-    l2_loss *= (weight_decay/2.)
-    return l2_loss
+def l1_term(model, weight_decay):
+    loss = sum([l.norm(1) for l in model.parameters()])
+    return weight_decay * loss
 
 # L2 regularization
-def l2_loss(model,model_init,weight_decay):
-    l2_loss = 0
-    for (k,p),(k_init,p_init) in zip(model.named_parameters(), model_init.named_parameters()):
+def l2_term(model,model_init,weight_decay):
+    loss = 0
+    for (_, x), (_, x0) in zip(model.named_parameters(), model_init.named_parameters()):
         if p.requires_grad:
-            l2_loss +=  (p-p_init).pow(2).sum()
-    l2_loss *= (weight_decay/2.)
-    return l2_loss
+            loss +=  ((x-x0) *  (x-x0)).sum()
+    return loss * (weight_decay / 2.)
+
 
 def configure_learning_rate(optimizer, epoch):
+    pass
+
+def fit():
     pass
